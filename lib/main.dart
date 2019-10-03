e5342461b3ff1eae04d0fb689758dbbac792e397
Jonathan Mtz <rockerjony@gmail.com>
2019-10-03 14:02:48 -0500

    code clean, more visual tricks

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget{
       theme: ThemeData.dark(),
       home: Container(
         child: Scaffold(
+          resizeToAvoidBottomInset: false,
           appBar: AppBar(
             title: Row(
               mainAxisSize: MainAxisSize.min,
@@ -36,25 +37,76 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        children: <Widget>[
-          dayWidget("Lunes", Icons.wb_sunny,"32C"),
-          dayWidget("Martes", Icons.wb_sunny,"30C"),
-          dayWidget("Miercoles", Icons.wb_cloudy,"16C"),
-          dayWidget("Jueves", Icons.grain,"10C"),
-          dayWidget("Viernes", Icons.wb_sunny,"32C"),
-
-        ],
-      )
-    );
+    return (Column(
+      children: <Widget>[
+        header(),
+        listWidget()
+      ],
+    ));
   }
 
+
+  Widget header(){
+    return ( Stack(
+      children: <Widget>[
+        Container(
+          height: 100.0,
+          child:Image.network("https://www.visitmexico.com/viajemospormexico/assets/uploads/destinos/zacatecas_destinos-principales_zacatecas_01.jpg",
+            fit: BoxFit.cover,
+            width: 850
+            ,
+          )
+
+
+        ),
+        Container(
+          height: 100.0,
+          width: 850,
+          color: Colors.black26,
+        ),
+        Positioned(
+          child: Text("Zacatecas",
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          left: 16.0,
+          top: 16.0,
+        ),
+        Positioned(
+          right: 8.0,
+          bottom: 8.0,
+          child: Text("10C",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              )),
+
+        )
+      ],
+    ));
+  }
+
+  Widget listWidget(){
+    return Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            dayWidget("Lunes", Icons.wb_sunny,"32C"),
+            dayWidget("Martes", Icons.wb_sunny,"30C"),
+            dayWidget("Miercoles", Icons.wb_cloudy,"16C"),
+            dayWidget("Jueves", Icons.grain,"10C"),
+            dayWidget("Viernes", Icons.wb_sunny,"32C"),
+
+          ],
+        )
+    );
+  }
   Widget dayWidget(String day, IconData icono, String temp){
     return Card(
       child: Padding(
-        padding: EdgeInsets.all(24.0),
+        padding: EdgeInsets.all(20.0),
         child: Row(
           mainAxisAlignment:MainAxisAlignment.spaceEvenly ,
           children: <Widget>[
